77f989d16be98fa8f3ac348d8901c910485154c7
alisonjin <aqjin@andrew.cmu.edu>
2023-12-08 00:11:11 -0500

    flextest

---
+++ flextest/faultsim.tcl
@@ -0,0 +1,18 @@
+add clocks 0 CK
+
+set test cycle 2
+setup pin strobes 1
+
+set system mode fault
+
+set pattern source external ../tessent/s9234_scan.ascii
+
+set hypertrophic limit off
+load faults s9234.faults
+
+run
+
+report statistics
+write faults s9234_faults.rpt -replace
+
+exit
